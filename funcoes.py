a1ec02cd1f44518db8d27f3a27264e30e73c965b
Laura Tovo <lau.tovoc@gmail.com>
2022-05-11 10:29:27 -0300

    passar fórmula

--- funcoes.py
+++ funcoes.py
@@ -24,3 +24,29 @@ def haversine(raio, latitude1, longitude1, latitude2, longitude2):
     distancia= 2*raio*(asin(funcao))
     return distancia
 
+
+import random
+def sorteia_letra(string,lista_restritas):
+    #deixar tudo minusculo
+    palavra=''
+    string = string.lower()
+    for c in string:
+        if c.isalnum():
+            palavra+=c
+    
+    sorteio=[]
+    letras=[]
+
+    for l in palavra:
+        letras.append(l)
+        
+    sorteada=random.choice(letras)
+    
+    if sorteada in lista_restritas:
+        i=0
+        while i < len(letras):
+            sorteada=random.choice(letras)
+            i+=1
+    if sorteada in lista_restritas:
+        sorteada=''
+    return sorteada
