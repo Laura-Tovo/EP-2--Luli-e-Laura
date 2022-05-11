6398c122a20195896200a607ca36a29182984a24
Laura Tovo <lau.tovoc@gmail.com>
2022-05-11 10:32:55 -0300

    passar fórmulas 2

--- funcoes.py
+++ funcoes.py
@@ -50,3 +50,57 @@ def sorteia_letra(string,lista_restritas):
     if sorteada in lista_restritas:
         sorteada=''
     return sorteada
+
+def esta_na_lista(pais,lista):
+    cond=True
+    lista_limpa=[]
+    for item in lista:
+        lista_limpa.extend(item)
+
+    if pais not in lista_limpa:
+        cond=False
+    if pais in lista_limpa:
+        cond=True
+    return cond
+
+def adiciona_em_ordem(pais,distancia,lista):
+    pais_novo=[pais,distancia]
+    if pais_novo in lista:
+        condicao = len(lista)-1
+    else:
+        condicao = len(lista)
+    if lista == []:
+        lista_nova = [pais_novo]
+    else:
+        if len(lista)>1:
+            if pais_novo not in lista:
+                lista_nova=[0]*(len(lista)+1)
+            if pais_novo in lista:
+                lista_nova=[0]*len(lista)
+            i=0
+            while i < condicao:
+                if pais_novo[1] >= lista[i][1]:
+                    lista_nova[i]=lista[i]
+                if pais_novo not in lista_nova:
+                    if pais_novo[1] < lista[i][1]:
+                        lista_nova[i]=pais_novo
+                        i-=1
+                elif pais_novo in lista_nova:
+                    if pais_novo not in lista:
+                        lista_nova[i+1]=lista[i]
+                    else:
+                        lista_nova[i]=lista[i]
+                i+=1
+            if pais_novo not in lista_nova:
+                lista_nova[i]=pais_novo
+        if len(lista)==1:
+            lista_nova = [0]*2
+            if pais_novo[1] < lista[0][1]:
+                lista_nova[0] = pais_novo
+                lista_nova[1] = lista[0]
+            else:
+                lista_nova[0] = lista[0]
+                lista_nova[1] = pais_novo                
+                
+            
+    return lista_nova
